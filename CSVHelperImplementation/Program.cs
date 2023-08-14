3fe3acfb05442d2aa827e9139664ee384c3e2255
Riya Susan Reji <riyasusan.r@tvsnext.com>
2023-08-14 11:35:08 +0530

    [Riya] Add : CSVhelper and Json.NET

--- CSVHelperImplementation/Program.cs
+++ CSVHelperImplementation/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace CSVHelperImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CSVHandlerImplement cSVHandlerImplement = new CSVHandlerImplement();
+            cSVHandlerImplement.ImplementCSVHandler();
+            cSVHandlerImplement.ImplementCSVHandlerToJson();
+            cSVHandlerImplement.ImplementJsonToCsv();
+        }
+    }
+}
